2ef9ff97858b68ec30a263db56289804cb685696
introkun <introkun@gmail.com>
2021-01-19 00:36:42 +0300

    version bumped v0.1.4

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(os.path.join(HERE, "README.md")) as fid:
 # This call to setup() does all the work
 setuptools.setup(
 	name="qt-range-slider",
-	version="0.1.3",
+	version="0.1.4",
 	description="Qt widget-slider with two thumbs (min/max values)",
 	long_description=README,
 	long_description_content_type="text/markdown",
