ab4509c187b852b8f88788fcc11b49fea1a7934c
introkun <introkun@gmail.com>
2021-01-21 23:30:35 +0300

    updated example and demo image

--- example.py
+++ example.py
@@ -18,8 +18,9 @@ def _render_slider_with_labels(parent, layout, min_value = 0, max_value = 10, \
 	left_thumb_value=0, right_thumb_value=None, size_value=False):
 	inner_layout = QHBoxLayout(parent)
 
-	label_min = QLabel(size(min_value) if size_value else str(min_value))
-	label_max = QLabel(size(max_value) if size_value else str(max_value))
+	label_min = QLabel(size(left_thumb_value) if size_value else str(left_thumb_value))
+	_right_thumb_value = right_thumb_value if right_thumb_value else max_value
+	label_max = QLabel(size(_right_thumb_value) if size_value else str(_right_thumb_value))
 	slider = QtRangeSlider(parent, min_value, max_value, left_thumb_value, right_thumb_value)
 	inner_layout.addWidget(label_min)
 	inner_layout.addWidget(slider)
