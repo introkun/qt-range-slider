b742fd351e032426d4ff077fe146b9bb8054ae4d
introkun <introkun@gmail.com>
2021-01-22 00:03:20 +0300

    fixed qt warnings

--- example.py
+++ example.py
@@ -7,21 +7,21 @@ from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QLa
 from qt_range_slider import QtRangeSlider
 
 # pylint: disable=too-many-arguments
-def _render_only_slider(parent, layout, min_value = 0, max_value = 10):
-	inner_layout = QHBoxLayout(parent)
-	slider = QtRangeSlider(parent, min_value, max_value)
+def _render_only_slider(layout, min_value = 0, max_value = 10):
+	inner_layout = QHBoxLayout()
+	slider = QtRangeSlider(layout.parent(), min_value, max_value)
 	inner_layout.addWidget(slider)
 
 	layout.addLayout(inner_layout)
 
-def _render_slider_with_labels(parent, layout, min_value = 0, max_value = 10, \
+def _render_slider_with_labels(layout, min_value = 0, max_value = 10, \
 	left_thumb_value=0, right_thumb_value=None, size_value=False):
-	inner_layout = QHBoxLayout(parent)
+	inner_layout = QHBoxLayout()
 
 	label_min = QLabel(size(left_thumb_value) if size_value else str(left_thumb_value))
 	_right_thumb_value = right_thumb_value if right_thumb_value else max_value
 	label_max = QLabel(size(_right_thumb_value) if size_value else str(_right_thumb_value))
-	slider = QtRangeSlider(parent, min_value, max_value, left_thumb_value, right_thumb_value)
+	slider = QtRangeSlider(layout.parent(), min_value, max_value, left_thumb_value, right_thumb_value)
 	inner_layout.addWidget(label_min)
 	inner_layout.addWidget(slider)
 	inner_layout.addWidget(label_max)
@@ -47,16 +47,16 @@ def main():
 
 	min_value = 0
 	max_value = 10
-	_render_only_slider(main_window, layout, min_value, max_value)
-	_render_slider_with_labels(main_window, layout, min_value, max_value)
-	_render_slider_with_labels(main_window, layout, min_value, max_value, min_value + 1, \
+	_render_only_slider(layout, min_value, max_value)
+	_render_slider_with_labels(layout, min_value, max_value)
+	_render_slider_with_labels(layout, min_value, max_value, min_value + 1, \
 		max_value - 1)
 	min_value = 0
 	max_value = 10*1024*1024*1024
-	_render_slider_with_labels(main_window, layout, min_value, max_value, max_value // 3, \
+	_render_slider_with_labels(layout, min_value, max_value, max_value // 3, \
 		max_value * 3 // 4, size_value=True)
 
-	slider = _render_slider_with_labels(main_window, layout, min_value, max_value, max_value // 3, \
+	slider = _render_slider_with_labels(layout, min_value, max_value, max_value // 3, \
 		max_value * 3 // 4, size_value=True)
 	slider.set_ticks_count(10)
 
